f107cf917f4e27bc67d579c023c509a52481ca82
chida876 <116781445+chida876@users.noreply.github.com>
2024-05-06 16:16:58 +0530

    Update delete_old_snapshots.sh

--- delete_old_snapshots.sh
+++ delete_old_snapshots.sh
@@ -2,9 +2,11 @@
 
 # Variables
 DAYS_TO_KEEP=14
-VOLUME_ID="your-volume-id"
+VOLUME_ID="vol-0dc593a3d860c22e0"
 
-# Delete old snapshots
-aws ec2 describe-snapshots --filters "Name=volume-id,Values=$VOLUME_ID" | \
-jq -r '.Snapshots[] | select(.StartTime <= "'$(date -d -$DAYS_TO_KEEP' days ago' + "%Y-%m-%d")'") | .SnapshotId' | \
+# Get current date in YYYY-MM-DD format
+CUTOFF_DATE=$(date -d "-$DAYS_TO_KEEP days" +%Y-%m-%d)
+
+# Describe snapshots and delete old ones
+aws ec2 describe-snapshots --filters "Name=volume-id,Values=$VOLUME_ID" --query "Snapshots[?StartTime<'$CUTOFF_DATE'].SnapshotId" --output text | \
 xargs -r -n 1 -t aws ec2 delete-snapshot --snapshot-id
